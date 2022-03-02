9707afecd20e4b0bc223e0790d8021a56f5de1b5
Farhad Azadaliyev <farhadja@code.edu.az>
2022-03-03 01:36:00 +0400

    initial

---
+++ Introduction/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Introduction.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            /*     return Json(new
+                 {
+                     name ="Farhad",
+                     surname="Azadaliyev"
+                 });*/
+
+
+            return View();
+        }
+
+
+        /*public string Test()
+        {
+            return "salam";
+        }*/
+    }
+}
